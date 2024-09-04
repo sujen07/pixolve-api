90d41a03fa5f2fc5a968fba101f4286838ef2e52
sujen07 <sujenk@gmail.com>
2024-09-03 22:53:48 -0700

    change get models script to use google drive

--- get_models.py
+++ get_models.py
@@ -1,31 +1,32 @@
 import os
 import requests
 
-# URL of the directory containing the files
-base_url = 'https://filebrowser-production-a7c5.up.railway.app/files/'
+# Google Drive file IDs
+file_ids = {
+    'shape_predictor_68_face_landmarks.dat': '1ezlZygd4SQpQq-N4VgCk7rXA8Sp9L3p0',
+    'dlib_face_recognition_resnet_model_v1.dat': '1eM58gYNu1xRmf5bBySQwYO-sWPwc0AE7'
+}
 
-# List of filenames to check and download
-filenames = ['shape_predictor_68_face_landmarks.dat', 'dlib_face_recognition_resnet_model_v1.dat']  # replace with your filenames
-
-def download_file(filename):
-    url = base_url + filename
+def download_file(filename, file_id):
+    url = f'https://drive.google.com/uc?export=download&id={file_id}'
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open(filename, 'wb') as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
-        print(f"Downloaded and replaced {filename}")
+        print(f"Downloaded and saved {filename}")
     else:
         print(f"Failed to download {filename}. Status code: {response.status_code}")
 
-def check_and_replace_files(filenames):
-    for filename in filenames:
+def check_and_replace_files(file_ids):
+    for filename, file_id in file_ids.items():
         if os.path.exists(filename):
             print(f"{filename} exists. Replacing...")
-            download_file(filename)
+            download_file(filename, file_id)
         else:
             print(f"{filename} does not exist. Downloading...")
-            download_file(filename)
+            download_file(filename, file_id)
 
 if __name__ == "__main__":
-    check_and_replace_files(filenames)
+    check_and_replace_files(file_ids)
+
